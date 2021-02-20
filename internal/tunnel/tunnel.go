23b0e5cf1a9676628372884716a4f4570818822b
echicken <echicken@bbs.electronicchicken.com>
2021-02-20 13:51:55 -0500

    Move rlogin client init stuff into func

--- internal/tunnel/tunnel.go
+++ internal/tunnel/tunnel.go
@@ -12,36 +12,55 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-// Start an SSH tunnel
-func Start(localConn net.Conn, cfg config.Config) {
+type rLoginClient struct {
+	password string
+	userName string
+	termType string
+}
 
-	defer localConn.Close()
-
+func rloginInit(localConn net.Conn, cfg config.Config) (*rLoginClient, error) {
 	localConn.Write([]byte("\x00"))
 
 	// Read the RLogin client username, server username, and termtype parameters from the client
-	rloginInit := make([]byte, 512)
-	_, err := localConn.Read(rloginInit)
+	rlb := make([]byte, 512)
+	_, err := localConn.Read(rlb)
 	if err != nil {
-		log.Printf("Error reading initial RLogin data from local client: %v %v", rloginInit, err)
-		return
+		return nil, err
 	}
 	localConn.Write([]byte("\x00"))
 
 	// Slice of "", client username, server username, terminal-type
-	rloginData := strings.Split(string(rloginInit), "\x00")
+	rloginData := strings.Split(string(rlb), "\x00")
 
-	userName := ""
+	rlc := rLoginClient{
+		password: rloginData[1],
+		termType: rloginData[3],
+	}
+
 	userNameRe := regexp.MustCompile(`^\[.+\].+`)
 	// If the RLogin username contains a system tag, leave it as is
 	if userNameRe.MatchString(rloginData[2]) {
-		userName = rloginData[2]
+		rlc.userName = rloginData[2]
 		// Otherwise, prefix it with the system tag from the config file
 	} else {
 		// Strip any square brackets from the config file's system_tag value
 		systemTagRe := regexp.MustCompile(`\[|\]`)
 		systemTag := systemTagRe.ReplaceAllString(cfg.SystemTag, "")
-		userName = fmt.Sprintf("[%s]%s", systemTag, rloginData[2])
+		rlc.userName = fmt.Sprintf("[%s]%s", systemTag, rloginData[2])
+	}
+
+	return &rlc, nil
+}
+
+// Start an SSH tunnel
+func Start(localConn net.Conn, cfg config.Config) {
+
+	defer localConn.Close()
+
+	rlc, err := rloginInit(localConn, cfg)
+	if err != nil {
+		log.Printf("Error reading initial RLogin data from local client: %v", err)
+		return
 	}
 
 	// Connect to the SSH server and authenticate
@@ -54,29 +73,29 @@ func Start(localConn net.Conn, cfg config.Config) {
 		},
 	}
 	sshHost := fmt.Sprintf("%s:%s", cfg.SSHHost, cfg.SSHPort)
-	log.Printf("%s connecting to SSH server %s", userName, sshHost)
+	log.Printf("%s connecting to SSH server %s", rlc.userName, sshHost)
 	serverConn, err := ssh.Dial("tcp", sshHost, sshConfig)
 	if err != nil {
-		log.Printf("%s encountered error connecting to SSH server: %v", userName, err)
+		log.Printf("%s encountered error connecting to SSH server: %v", rlc.userName, err)
 		return
 	}
 	defer serverConn.Close()
-	log.Printf("%s connected to SSH server %s", userName, sshHost)
+	log.Printf("%s connected to SSH server %s", rlc.userName, sshHost)
 
 	// Connect to the RLogin server via the SSH connection
 	rloginHost := fmt.Sprintf("%s:%s", cfg.RLoginHost, cfg.RLoginPort)
-	log.Printf("%s connecting to RLogin server %s via SSH tunnel", userName, rloginHost)
+	log.Printf("%s connecting to RLogin server %s via SSH tunnel", rlc.userName, rloginHost)
 	remoteConn, err := serverConn.Dial("tcp", rloginHost)
 	if err != nil {
-		log.Printf("%s encountered error connecting to RLogin server via SSH tunnel: %v", userName, err)
+		log.Printf("%s encountered error connecting to RLogin server via SSH tunnel: %v", rlc.userName, err)
 		return
 	}
 	defer remoteConn.Close()
-	log.Printf("%s connected to RLogin server %s via SSH tunnel", userName, rloginHost)
+	log.Printf("%s connected to RLogin server %s via SSH tunnel", rlc.userName, rloginHost)
 
 	// "Authenticate" and begin the actual RLogin session through the tunnel
-	log.Printf("%s starting RLogin session, terminal type: %s", userName, rloginData[3])
-	fmt.Fprintf(remoteConn, "\x00%s\x00%s\x00%s\x00", rloginData[1], userName, rloginData[3])
+	log.Printf("%s starting RLogin session, terminal type: %s", rlc.userName, rlc.termType)
+	fmt.Fprintf(remoteConn, "\x00%s\x00%s\x00%s\x00", rlc.password, rlc.userName, rlc.termType)
 
 	go func() {
 		io.Copy(remoteConn, localConn)
@@ -84,6 +103,6 @@ func Start(localConn net.Conn, cfg config.Config) {
 	io.Copy(localConn, remoteConn)
 
 	localConn.Close()
-	log.Printf("%s disconnected", userName)
+	log.Printf("%s disconnected", rlc.userName)
 
 }
